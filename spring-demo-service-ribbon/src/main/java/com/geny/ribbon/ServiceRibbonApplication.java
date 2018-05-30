057c6a9f9d392bea188ce1baa1af20a905c22cc0
hubo <hubsh@sinosoft.com.cn>
2018-05-30 16:03:33 +0800

    添加 ribbon 熔断器

--- spring-demo-service-ribbon/src/main/java/com/geny/ribbon/ServiceRibbonApplication.java
+++ spring-demo-service-ribbon/src/main/java/com/geny/ribbon/ServiceRibbonApplication.java
@@ -2,6 +2,7 @@ package com.geny.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
--- spring-demo-service-ribbon/src/main/java/com/geny/ribbon/SpringDemoRibbonService.java
+++ spring-demo-service-ribbon/src/main/java/com/geny/ribbon/SpringDemoRibbonService.java
@@ -1,5 +1,6 @@
 package com.geny.ribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,12 @@ public class SpringDemoRibbonService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "portFallback")
     public String port() {
         return restTemplate.getForObject("http://SPRING-DEMO-SERVICE/port", String.class);
+    }
+
+    public String portFallback() {
+        return "sorry ribbon, it's error!";
     }
 }
