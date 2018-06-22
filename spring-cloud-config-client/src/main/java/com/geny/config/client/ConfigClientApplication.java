e49fa19b1b094126fcb3048f20215e5d20f5e60e
hubo <hubsh@sinosoft.com.cn>
2018-06-22 17:52:28 +0800

    添加 Spring Cloud Bus，动态刷新 Config

--- spring-cloud-config-client/src/main/java/com/geny/config/client/ConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/geny/config/client/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.geny.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
