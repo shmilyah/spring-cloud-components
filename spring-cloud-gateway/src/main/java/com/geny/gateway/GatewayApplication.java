042e601ab5a3ec63809e759fc2efccab4d63ae54
hubo <hubo89@126.com>
2018-06-28 22:38:19 +0800

    添加服务网关 Zuul 模块

---
+++ spring-cloud-gateway/src/main/java/com/geny/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.geny.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author shmilyahu
+ * @date 2018/6/28 20:22
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
